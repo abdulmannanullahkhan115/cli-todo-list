72a2286126192da551ad713cba2a30a29dc7dfaa
abdul mannan ullah khan <abdulmannanullahkhan115@gmail.com>
2024-06-05 13:27:04 +0500

    created a todo app

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: "input",
+            message: 'eid ki kia shopping ki hai'
+        },
+        {
+            name: 'addmore',
+            type: "confirm",
+            message: "aur kuch likhna hai?",
+            default: 'false',
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask['addmore'];
+    console.log(todos);
+}
